6cceef9a37fe64de12568a6758c1117bb2fc22b8
VarunKumar988 <cvarunkumar455@gmail.com>
2026-01-14 22:46:20 +0530

    chore: auto-deploy updates

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    BACKEND_URL: process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:8000/api",
+    BACKEND_URL: process.env.REACT_APP_BACKEND_URL || "https://ai-tools-vho6.vercel.app/api",
 };
 
 export default config;
